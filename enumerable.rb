3366dd7c3ff547e9c7b63215a32f27baae982e70
Gabriela Cruz <viridiana.cruz.olivera@gmail.com>
2019-07-17 12:50:01 -0700

    my-all method added

--- enumerable.rb
+++ enumerable.rb
@@ -1,23 +1,43 @@
 module Enumerable
-  def my_each(arr)
+  def my_each
     #code here
-    n = arr.length
+    n = self.length
     for i in 0...n
-      element = arr[i]
+      element = self[i]
       yield(element)
     end
     
   end
+
+  def my_all?
+    val = true
+    self.my_each { |element| return false if yield(element) == false }
+    val
+  end
+
 end
 
-class MyMethods
-  include Enumerable
+include Enumerable
+
+print [nil, true, 99].my_all?
+
+# class MyMethods
+#   include Enumerable
   
-  def initialize; end
-end
+#   def initialize; end
+# end
 
-each = MyMethods.new
-each.my_each([2, 3, 4]) { |param| puts "my each element #{param}"}
+# each = MyMethods.new
+# #print each.my_each([2, 3, 4]) { |param| param.even? }
 
+# #print [2, 3, 4].each { |param| param.even? }
+# #print [2, 4].all? { |param| param.even? }
+# arr_s = %w(ant bear cat)
+# puts each.my_all?([1, 2i, 3.14])
 
-[2, 3, 4].each { |param| puts "element #{param}" }
+# #puts %w[ant bear cat].all? { |word| word.length >= 3 } #=> true
+# #puts %w[ant bear cat].all? { |word| word.length >= 4 } #=> false
+# puts %w[ant bear cat].all?(/t/)                        #=> false
+# #puts [1, 2i, 3.14].all?(Numeric)                       #=> true
+# #puts [nil, true, 99].all?                              #=> false
+# #puts [].all?                                           #=> true
