3c49c91fb6590524b3e4fdfce04ad27fc3bb586f
Gabriela Cruz <viridiana.cruz.olivera@gmail.com>
2019-07-18 11:28:34 -0700

    my-map accepting a proc as an argument.

--- enumerable.rb
+++ enumerable.rb
@@ -9,9 +9,9 @@ module Enumerable
     
   end
 
-  def my_map
+  def my_map(proc)
     arr = []
-    self.to_a.my_each { |element| arr << yield(element) }
+    self.to_a.my_each { |element| arr << proc.call(element) }
     arr
   end
 end
@@ -21,6 +21,8 @@ include Enumerable
 proc = Proc.new {
   |element|
   element * element
-}
+} 
+#a proc can be stored in a variable and can be passed as a normal argument
 
-print (1..4).my_map(&proc)     #=> [1, 4, 9, 16]
+print (1..4).my_map(proc)   #=> [1, 4, 9, 16]
+#print (1..4).my_map { |e| e * e }
