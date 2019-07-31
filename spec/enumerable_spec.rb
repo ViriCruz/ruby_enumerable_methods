c2fa67dba04aaa6892f7f8b19066417dd05aff63
ViriCruz <viridiana.cruz.olivera@gmail.com>
2019-07-31 16:51:30 -0700

    added three examples for my_each method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -4,16 +4,29 @@ require_relative '../enumerable'
 describe Enumerable do 
   # write your examples here
   # testing my_each
-  context "when passing a unidimensional integer array to my_each method" do
-    # example here
-    it "my_each method should print every element of the array" do
-      arr = [2,3,4,5]
-      expect{arr.my_each { |x| print x }}.to output("2345").to_stdout
+  describe "#my_each" do
+    let!(:num_arr){[2, 3, 4, 5]}
+    let!(:str_arr){['a', 'b', 'c', 'd']}
+    context "when passing a unidimensional integer array" do
+      # example here
+      it do
+        expect{ num_arr.my_each { |x| print x } }.to output("2345").to_stdout
+      end
+    end
+  
+    context "when passing a unidimensional string array" do
+      # example here
+      it do
+        expect{ str_arr.my_each { |s| print s } }.to output("abcd").to_stdout
+      end
+    end
+
+    # edge case
+    context "when passing an empty array" do 
+      it do
+        expect( [].my_each { |e| print e } ).to match_array([])
+      end
     end
   end
-
-  context "when passing a unidimensional string array to my_each method" do
-    # example here
-  end
-
+  
 end
