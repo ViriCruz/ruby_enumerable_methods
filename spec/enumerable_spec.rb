8cc1071e9d5cb5532af53fc0bef3716d78ffd9a5
ViriCruz <viridiana.cruz.olivera@gmail.com>
2019-08-01 10:48:49 -0700

    positive examples and edge cases addede to test my_each_with_index

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,30 +1,75 @@
 require_relative '../enumerable'
 
 # collection of tests
+# Note: for raise error there are 3 possible errors: NoMethodError, NameError or ArgumentError, consider
+# provide a specific error when testing a negative escenario 
 describe Enumerable do 
   # write your examples here
+  let!(:str_arr){['a', 'b', 'c', 'd']}
+  let!(:num_arr){[2, 3, 4, 5]}
   # testing my_each
   describe "#my_each" do
-    let!(:num_arr){[2, 3, 4, 5]}
-    let!(:str_arr){['a', 'b', 'c', 'd']}
     context "when passing a unidimensional integer array" do
       # example here
       it do
-        expect{ num_arr.my_each { |x| print x } }.to output("2345").to_stdout
+        expect { num_arr.my_each { |x| print x } }.to output("2345").to_stdout
       end
     end
   
     context "when passing a unidimensional string array" do
       # example here
       it do
-        expect{ str_arr.my_each { |s| print s } }.to output("abcd").to_stdout
+        expect { str_arr.my_each { |s| print s } }.to output("abcd").to_stdout
       end
     end
 
     # edge case
     context "when passing an empty array" do 
       it do
-        expect( [].my_each { |e| print e } ).to match_array([])
+        expect([].my_each { |e| print e }).to match_array([])
+      end
+    end
+
+    # edge case
+    context "when passing a string" do
+      it "raises" do
+        expect { "1".my_each { |e| print e } }.to raise_error(NoMethodError)
+      end
+    end
+
+    # edge case
+    context "when passing a number" do
+      it "raises" do
+        expect { 1.my_each { |e| print e } }.to raise_error(NoMethodError)
+      end
+    end
+  end
+
+  describe "#my_each_with_index" do
+    context "when passing a string array" do
+      it do
+        expect { str_arr.my_each_with_index { |s, i| print "#{s}->#{i}" } }.to  output("a->0b->1c->2d->3").to_stdout
+      end
+    end
+
+    context "when passing a integer array" do
+      it do
+        expect { num_arr.my_each_with_index { |n, i| print "#{n}->#{i} " } }.to  output("2->0 3->1 4->2 5->3 ").to_stdout
+      end
+    end
+
+    #edge case
+    context "when passing an empty array" do
+      it do
+        expect([].my_each_with_index { |o, i| print o }).to match_array([])
+      end
+    end
+
+    #edge case
+    context "when passing a number" do
+      # raise error
+      it "raises" do
+        expect { 2.my_each_with_index { |e, i| print "#{e}->#{i}" } }.to raise_error(NoMethodError)
       end
     end
   end
